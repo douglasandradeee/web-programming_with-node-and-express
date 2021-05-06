df4547df42e85e075d020626c6571f693bf4c435
Douglas Andrade <dba12@hotmail.com>
2021-05-06 00:30:49 -0300

    CHAPTER-THREE

--- chapter-three/meadowlark_2.js
+++ chapter-three/meadowlark_2.js
@@ -3,17 +3,30 @@ const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need sprngs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
+
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public'))
+
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', { fortune: randomFortune })
+})
 
 // custom 404 page
 app.use((req, res) => {
